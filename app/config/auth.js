e5487154cd3a147a2f3ed5af187e13187ac0e8e4
Dustin Firebaugh <dafirebaugh@gmail.com>
2017-11-29 06:14:57 +0000

    updating config

--- app/config/auth.js
+++ app/config/auth.js
@@ -3,7 +3,8 @@
 module.exports = {
 	'githubAuth': {
 		'clientID': process.env.GITHUB_KEY,
-		'clientSecret': process.env.GITHUB_SECRET,
-		'callbackURL': process.env.APP_URL + 'auth/github/callback'
+		'clientSecret': process.env.GITHUB_SECRET//,
+		// 'callbackURL': process.env.APP_URL + 'auth/github/callback'
 	}
 };
+
